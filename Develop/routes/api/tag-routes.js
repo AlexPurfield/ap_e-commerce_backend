d20b33b10b97b68234eca637a265f6af1cc1d65c
Alex Purfield <alexpurfield@gmail.com>
2023-10-16 17:30:51 -0600

    routes done, first round of testing routes with insomnia

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -46,14 +46,45 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name,
+  })
+  .then((tag) => {
+    res.json(tag);
+  })
+  .catch((err) =>
+  res.json(err))
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
-});
+  Tag.update(req.body.tag_name,{
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((tag) => {
+    res.json(tag);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
+  
+  });
+
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({where: {
+    id:req.params.id,
+  },
+})
+.then((deletedTag) => {
+  res.json(`${deletedTag} tag were removed from the database`);
+})
+.catch((err) => {
+  res.json(err);
+});
 });
 
 module.exports = router;
