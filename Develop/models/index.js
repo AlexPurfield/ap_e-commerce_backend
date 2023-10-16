6bb9091c9dc3f77b3a28fe8ca73793db56a885d5
Alex Purfield <alexpurfield@gmail.com>
2023-10-16 00:31:41 -0600

    added models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,12 +4,26 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category);
+  // Products belongsTo Category
 
+Category.hasMany(Product);
 // Categories have many Products
 
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'product_id'
+  }
+})
 // Products belongToMany Tags (through ProductTag)
 
+Tag.belongsToMany(Product, {
+  through: {
+    model:ProductTag,
+    foreignKey: 'tag_id'
+  }
+})
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
